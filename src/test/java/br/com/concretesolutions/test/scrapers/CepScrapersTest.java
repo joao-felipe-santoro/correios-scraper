51c57c8a900c1a5583a04e51a9548cc2904969f7
Joao Felipe Santoro Pinto <joao.felipe@concretesolutions.com.br>
2016-02-04 11:31:02 -0200

    addind tests to CEP scraper

--- src/test/java/br/com/concretesolutions/test/scrapers/CepScrapersTest.java
+++ src/test/java/br/com/concretesolutions/test/scrapers/CepScrapersTest.java
@@ -1,31 +1,57 @@
-/*
-* Copyright 2016 Redecard S.A.
-*************************************************************
-*Nome     : CepScrapersTest.java
-*Autor    : jfelipesp
-*Data     : 4 de fev de 2016
-*Empresa  : Concrete Solutions / Redecard
-*************************************************************
-*/
 package br.com.concretesolutions.test.scrapers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import br.com.concretesolutions.api.models.CepResult;
 import br.com.concretesolutions.scrapers.CepScraper;
 
 import org.junit.Test;
 
+// TODO: Auto-generated Javadoc
 /**
- * @author jfelipesp
+ * The Class CepScrapersTest.
  *
+ * @author jfelipesp
  */
 public class CepScrapersTest {
   
+  /**
+   * Postalcode not found test.
+   *
+   * @throws Exception the exception
+   */
   @Test(expected = IllegalStateException.class)
-  public void testListSecurityKeysSuccess() throws Exception{
+  public void postalcodeNotFoundTest() throws Exception {
     
+    CepScraper.getPostalcodeResult("99999999");
+  }
+
+  /**
+   * Postalcode test.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void postalcodeTest() throws Exception {
     
-    CepResult result =  CepScraper.getPostalcodeResult("99999999");
+    CepResult result = CepScraper.getPostalcodeResult("04564001");
     
+    assertEquals("Rua Pensilvânia", result.getStreet());
+    assertEquals("de 421/422 a 599/600 ", result.getNumberRange());
+    assertEquals("Cidade Monções ", result.getDistrict());
+    assertEquals("São Paulo", result.getCity());
+    assertEquals("SP", result.getState());
+    assertEquals("04564001", result.getPostalCode());
+    
+    result = CepScraper.getPostalcodeResult("20241320");
+    
+    assertEquals("Rua Joaquim Murtinho ", result.getStreet());
+    assertNull(result.getNumberRange());
+    assertEquals("Santa Teresa ", result.getDistrict());
+    assertEquals("Rio de Janeiro", result.getCity());
+    assertEquals("RJ", result.getState());
+    assertEquals("20241320", result.getPostalCode());
   }
 
 }

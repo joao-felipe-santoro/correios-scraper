b3c761b78c5cde9bff46944e10c1022cbe6219ff
Joao Felipe Santoro Pinto <joao.felipe@concretesolutions.com.br>
2016-02-05 21:04:19 -0200

    Removing unused imports

--- src/test/java/br/com/concretesolutions/test/scrapers/SroScrapersTest.java
+++ src/test/java/br/com/concretesolutions/test/scrapers/SroScrapersTest.java
@@ -20,7 +20,6 @@ import com.google.common.base.Charsets;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;

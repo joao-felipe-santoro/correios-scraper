bc743a94889a5e532747ab45a8b98015e333cf52
Joao Felipe Santoro Pinto <joao.felipe@concretesolutions.com.br>
2016-02-06 01:45:58 -0200

    in hope it will finally work

--- install-jpm4j.sh
+++ install-jpm4j.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo 'downloading jpm4j...'
 curl -sL http://bit.ly/jpm4j >jpm4j.jar
+echo 'DONE!'
+echo 'installing it'
 sudo java -jar jpm4j.jar -g init
-jpm install com.codacy:codacy-coverage-reporter:assembly
+echo 'DONE!'
+sudo jpm install com.codacy:codacy-coverage-reporter:assembly

eb3db3ebecfe6177a821f031c4b67eb36372fea2
Joao Felipe Santoro Pinto <joao.felipe@concretesolutions.com.br>
2016-06-15 13:13:48 -0300

    updating jpm4j script

--- install-jpm4j.sh
+++ install-jpm4j.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'downloading jpm4j...'
-curl -sL http://bit.ly/jpm4j >jpm4j.jar
+curl -sL curl -sL https://github.com/jpm4j/jpm4j.installers/raw/master/dist/biz.aQute.jpm.run.jar >jpm4j.jar
 echo 'DONE!'
 echo 'installing it'
 sudo java -jar jpm4j.jar -g init

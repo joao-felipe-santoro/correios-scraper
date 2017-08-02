ac73df839a4ce16c267300a63ad20c8dd042d6f8
Joao Felipe Santoro Pinto <joao.felipe@concretesolutions.com.br>
2017-08-02 17:41:27 -0300

    Fixing dependency package

--- src/main/java/br/com/concretesolutions/config/ApplicationConfig.java
+++ src/main/java/br/com/concretesolutions/config/ApplicationConfig.java
@@ -2,7 +2,7 @@ package br.com.concretesolutions.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;

fec4cc64367696b822e8653df636289e92632c4f
Joao Felipe Santoro Pinto <joao.felipe@concretesolutions.com.br>
2017-08-02 17:59:48 -0300

    Updating Codecov process.

--- install-jpm4j.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'downloading jpm4j...'
-curl -sL curl -sL https://github.com/jpm4j/jpm4j.installers/raw/master/dist/biz.aQute.jpm.run.jar >jpm4j.jar
-echo 'DONE!'
-echo 'installing it'
-sudo java -jar jpm4j.jar -g init
-echo 'DONE!'
-sudo jpm install com.codacy:codacy-coverage-reporter:assembly
